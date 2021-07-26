e47081697f54244108d9f4fe85e40048ee24b10c
xzh <995194437@qq.com>
2021-07-27 00:49:35 +0800

    feat：主体功能修改

--- index.js
+++ index.js
@@ -2,21 +2,22 @@
  * @Author: xizh 
  * @Date: 2021-07-21 22:57:14 
  * @Last Modified by: xizh
- * @Last Modified time: 2021-07-24 23:22:47
+ * @Last Modified time: 2021-07-27 00:44:15
  */
 
 'use strict'
 
-const path = require('path');
+const path = require('path')
 const fs = require('fs')
 
-const chalk = require('chalk');
-const figlet = require('figlet');
-const ora = require('ora');
-const inquirer = require('inquirer');
-
-const {createFolder, createFile, isObject} = require('./utils/index');
+const chalk = require('chalk')
+const figlet = require('figlet')
+const ora = require('ora')
+const inquirer = require('inquirer')
 const downloadRepo = require('download-git-repo')
+const execa = require('execa')
+
+const {createFolder, createFile, isObject} = require('./utils/index')
 
 class XizhCli {
     constructor(options){
@@ -98,7 +99,16 @@ class XizhCli {
         inquirer.prompt(this.questions).then(res => {
             this.validateConfig(res).then(res => {
                 this.config = res;
-                this.downloadTemplate()
+                this.downloadTemplate().then(flag => {
+                    if(flag){
+                        this.writeConfig()
+                        return flag
+                    }
+                })
+            }).then(res => {
+                if(res){
+                    this.installDependencies()
+                }
             }).catch(err => {
                 console.log('输入参数有误',err.msg)
             })
@@ -107,26 +117,28 @@ class XizhCli {
         })
 
     }
+    // 从git下载模板工程
     downloadTemplate = () => {
         // 1. download git 仓库
         const spinner = ora('downloading template').start();
         spinner.color = 'blue'
-
-        downloadRepo(`github:xzh97/vue-project-template`, `${__dirname}/${this.config.name}/`, err => {
-            spinner.stop()
-            if(!err){
-                // 把自定义的内容写入模板内
-                console.log(chalk.blue(`downloading template success}`))
-                this.writeConfig()
-
-            }
-            else{
-                console.log(chalk.blue('download template error'))
-                console.log(err)
-            }
+        return new Promise((resolve, reject) => {
+            downloadRepo(`github:xzh97/vue-project-template`, `${__dirname}/${this.config.name}/`, err => {
+                spinner.stop()
+                if(!err){
+                    // 把自定义的内容写入模板内
+                    console.log(chalk.blue(`downloading template success`))
+                    resolve(!err)
+                }
+                else{
+                    console.log(chalk.blue('download template error'))
+                    console.log(err)
+                    reject(err)
+                }
+            })
         })
-        
     }
+    // 验证前面的输入
     validateConfig(res){
         return new Promise((resolve, reject) => {
             this.rules.forEach(item => {
@@ -153,49 +165,44 @@ class XizhCli {
             })
         })
     }
-    writeConfig(){
-        let { name, description, author } = this.config;
-        let packageJsonPath = path.resolve(__dirname, name)
-        let packageJSON = require(packageJsonPath)
-        console.log(packageJSON);
 
+    // 把名称，描述，author写入到下载的git工程里的package.json
+    writeConfig(){
+        try{
+            let { name, description, author } = this.config;
+            let packageJsonPath = path.resolve(__dirname, name, 'package.json')
+            let packageJSON = require(packageJsonPath)
+    
+            if(packageJSON){
+                packageJSON.name = name
+                packageJSON.description = description
+                packageJSON.author = author
+            }
+    
+            let packageJSONStrData = JSON.stringify(packageJSON, undefined, 4)
+    
+            fs.writeFile(packageJsonPath, packageJSONStrData, (err) => {
+                if(err){
+                    console.log('fs.writeFile error',err)
+                }
+            })
+        }
+        catch(err){
+            console.log('写入package.json配置出错')
+        }
+    }
+
+    // 安装node_modules 即执行npm i
+    installDependencies(){
+        try{
+            let runPath = path.resolve(__dirname, name)
+            execa(runPath, 'npm install', { stdio: 'inherit' })
+            
+        }catch(err){
+            console.log('安装依赖出错')
+            console.log(err)
+        }
     }
 }
 
-// const createDir = (project, config, filepath = '') => {
-//     // let curPath = `${project}/${filepath}`;
-//     let curPath = path.resolve(project, filepath);
-//     console.log('createDir path-------',curPath);
-//     config.map(item => {
-//         if(isObject(item)){
-//             let p = path.resolve(curPath, item.name)
-//             if(item.isFile){
-//                 createFile(p)
-//                 // .catch(err => {
-//                 //     console.log(`新建${item.name}文件出现错误`)
-//                 // })
-//             }
-//             else{
-//                 createFolder(p).then(res => {
-//                     if(item.children && item.children.length){
-//                         let dirPath = `${item.name}/`;
-//                         createDir(curPath, item.children, dirPath);
-//                     }
-//                 })
-//                 // .catch(err => {
-//                 //     console.log(`新建${item.name}目录出现错误`)
-//                 // })
-//             }
-//         }
-//         else{
-//             let p = path.resolve(curPath, item)
-//             createFile(p)
-//             // .catch(err => {
-//             //     console.log(`新建${item}文件出现错误`)
-//             // })
-//         }
-//     })
-// }
-
-const demo = new XizhCli();
-demo.run();
+module.exports = XizhCli
